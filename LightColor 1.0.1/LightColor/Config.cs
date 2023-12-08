3e346ffea0fba4872c43dfdb8e485b17a9795c35
Treaxy <146599871+Treaxy@users.noreply.github.com>
2023-12-09 00:13:03 +0300

    Add files via upload

---
+++ LightColor 1.0.1/LightColor/Config.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+using UnityEngine;
+
+namespace LightColor
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin is Enabled?")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Debug is Enabled?")]
+        public bool Debug { get; set; } = false;
+        public Dictionary<string, Color> Colors { get; set; } = new Dictionary<string, Color>()
+        {
+            { "red", Color.red },
+            { "blue", Color.blue },
+            { "cyan", Color.cyan },
+            { "gray", Color.gray },
+            { "green", Color.green },
+            { "magenta", Color.magenta },
+            { "yellow", Color.yellow },
+            { "white", Color.white }
+        };
+    }
+}
