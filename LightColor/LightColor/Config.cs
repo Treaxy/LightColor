19c5123c71ec5c5fef0e2327b415b66b06bb850f
Treaxy <146599871+Treaxy@users.noreply.github.com>
2023-12-08 22:55:43 +0300

    LightColor Source

---
+++ LightColor/LightColor/Config.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Exiled.API.Interfaces;
+using RemoteAdmin;
+using UnityEngine;
+
+namespace LightColor
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin is Enabled?")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Debug is Enabled?")]
+        public bool Debug { get; set; } = false;
+        [Description("Setting of command name defeult ussage: lightcolor red")]
+        public string LightsRed { get; set; } = "red";
+        [Description("Setting of command name defeult ussage: lightcolor blue")]
+        public string LightsBlue { get; set; } = "blue";
+        [Description("Setting of command name defeult ussage: lightcolor green")]
+        public string LightsGreen { get; set; } = "green";
+        [Description("Setting of command name defeult ussage: lightcolor yellow")]
+        public string LightsYellow { get; set; } = "yellow";
+        [Description("Setting of command name defeult ussage: lightcolor gray")]
+        public string LightsGray { get; set; } = "gray";
+        [Description("Setting of command name defeult ussage: lightcolor cyan")]
+        public string LightsCyan { get; set; } = "cyan";
+        [Description("Setting of command name defeult ussage: lightcolor magenta")]
+        public string LightsMagenta { get; set; } = "magenta";
+        [Description("Setting of command name defeult ussage: lightcolor white")]
+        public string LightsDefault { get; set; } = "default";
+    }
+}
