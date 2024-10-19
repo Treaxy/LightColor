a007115115e230259d86a5d2c14185af6109f509
Treaxy <146599871+Treaxy@users.noreply.github.com>
2024-10-19 18:17:08 +0300

    Add files via upload

---
+++ LightColor 1.2.0/LightColor/ConsoleCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using CommandSystem;
+using Exiled.API.Features;
+using Exiled.Loader;
+using Exiled.Permissions.Extensions;
+using UnityEngine;
+
+namespace LightColor
+{
+    internal class ConsoleCommand
+    {
+        [CommandHandler(typeof(RemoteAdminCommandHandler))]
+
+        public class lightcolor : ICommand
+        {
+            public string Command => "LightColor";
+
+            public string[] Aliases => new[] { "lcc" };
+
+            public string Description => "Map Light Color changer";
+
+            public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+            {
+                if (!sender.CheckPermission("LightChanger"))
+                {
+                    response = "You need 'LightChanger' permission to use this command!";
+                    return false;
+                }
+                try
+                {
+                    foreach (RoomLightController light in RoomLightController.Instances)
+                    {
+                        if (light == null)
+                        {
+                            continue;
+                        }
+
+                        Map.ChangeLightsColor(Color.clear);
+                    }
+
+                    Color color = arguments.Count == 0
+                        ? Color.clear
+                        : Plugin.Instance.Config.Colors.TryGetValue(arguments.At(0), out Color col)
+                            ? col
+                            : Color.clear;
+
+                    Map.ChangeLightsColor(color);
+                    response = "Lights Successfuly changed";
+                    return true;
+                }
+                catch (Exception ex) 
+                {
+                    Log.Error("ConsoleCommand: "+ex.Message.ToString());
+                    response = "Error Occurred! Look your console to see error.";
+                    return false;
+                }
+            }
+        }
+    }
+}
